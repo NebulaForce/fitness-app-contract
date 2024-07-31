1057b9a975225d7a96ffeb8d57491266fca795cc
Lindsay Morales <lindsaymorales15@gmail.com>
2024-07-31 15:21:43 -0600

    Remove fitness single module, update env example

--- ignition/modules/FitnessTracking.js
+++
@@ -1,9 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("FitnessTracking", (m) => {
-
-  const FitnessTracking = m.contract("FitnessTracking", [], {
-  });
-
-  return { FitnessTracking };
-});
