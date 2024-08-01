bd44c1234b9180063c790fe94ad4a97ea205473a
Lindsay Morales <lindsaymorales15@gmail.com>
2024-08-01 02:05:27 -0600

    update deploy

--- ignition/modules/dynamicModule.js
+++ ignition/modules/dynamicModule.js
@@ -6,7 +6,7 @@ const { INITIAL_OWNER } = process.env;
 module.exports = buildModule("deployModule", (m) => {
   
   const contractDynamicNFT = m.contract("DynamicNFT", [INITIAL_OWNER]);
-  const contractFitness = m.contract("FitnessTracking", [contractDynamicNFT] );
+  const contractFitness = m.contract("FitnessTracking", [contractDynamicNFT, INITIAL_OWNER] );
 
 
   return { contractDynamicNFT, contractFitness };
